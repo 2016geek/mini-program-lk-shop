a24ced23f5b673d0b0969a82763cb29b1f02741b
majinbo <majinbo@dxy.cn>
2020-08-02 15:21:29 +0800

    feat: 添加统计页样式

---
+++ src/pages/bill/list/index.js
@@ -0,0 +1,33 @@
+import { BillStatusMap } from '../../../utils/bill';
+
+Page({
+	data: {
+		userInfo: {
+			sum: '100.00',
+			settle: '59.60',
+			unsettle: '40.40',
+		},
+		billList: [
+			{
+				'billAmount': 300000,
+				'billName': '张东升的账单',
+				'billPics': '',
+				'billTime': '2020.7.2',
+				'debtorName': '张东升',
+				'id': 1,
+				'settlementAmount': 12,
+				'status': 1,
+			},
+			{
+				'billAmount': 10130.2,
+				'billName': 'ZHENY的账单',
+				'billPics': '',
+				'billTime': '2020.7.1',
+				'debtorName': 'ZHENY',
+				'id': 2,
+				'settlementAmount': 9,
+				'status': 3,
+			},
+		],
+	},
+});
---
+++ src/utils/bill.js
@@ -0,0 +1,11 @@
+export const BillStatusMap = {
+	1: {
+		label: '待结算',
+	},
+	2: {
+		label: '计题中',
+	},
+	3: {
+		label: '已结算',
+	},
+};
