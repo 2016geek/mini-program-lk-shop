5518dc197778edf020e9b1c656779df683036a86
2016geek <2016geek@gmail.com>
2020-08-14 00:26:54 +0800

    fix -l 添加全局错误处理

--- src/api/createServer/index.js
+++ src/api/createServer/index.js
@@ -11,8 +11,19 @@ const createServer = (method, url) => {
 						'eyJhbGciOiJIUzI1NiJ9.eyJVU0VSX0lEIjoxLCJpYXQiOjE1OTczMDUyNTIsImV4cCI6MTU5NzkxMDA1Mn0.VSjxdOw8v90qZSl5qXUgEMDdOpa8JAapN2olstqbZJk',
 				},
 				success(res) {
-					console.log(res)
-					resolve(res.data)
+					const {
+						data,
+						data: { statusCode, errorMsg },
+					} = res
+					if (statusCode === 0) {
+						resolve(data)
+					} else {
+						wx.showToast({
+							title: errorMsg,
+							icon: 'none',
+						})
+						reject(Error(errorMsg))
+					}
 				},
 				fail(e) {
 					reject(e)
