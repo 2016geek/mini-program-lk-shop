fa97258d990d3441d88186b514a66ad1957d6d03
majinbo <majinbo@dxy.cn>
2020-08-15 21:47:35 +0800

    chore: 添加 api

--- src/api/bill.js
+++ src/api/bill.js
@@ -3,9 +3,10 @@ import createServer from './createServer';
 export default {
 	add: createServer('POST', '/accountbook/rest/bill/v1'),
 	amount: createServer('GET', '/accountbook/rest/bill/v1/amount'),
-	list: createServer('GET', '/accountbook/rest/bill/v1/list'),
+	list: createServer('POST', '/accountbook/rest/bill/v1/list'),
 	debtorAccrual: createServer('GET', '/accountbook/rest/bill/v1/debtor/accrual/list'),
-	debtorAmount: createServer('GET', '/accountbook/rest/bill/v1/amount/debtorId/2'),
+	debtorAmount: createServer('GET', '/accountbook/rest/bill/v1/amount/debtorId/{debtorId}'),
 	debtorMonthList: createServer('GET', '/accountbook/rest/bill/v1/debtor/month/list'),
 	debtorList: createServer('POST', '/accountbook/rest/bill/v1/debtor/list'),
+	addPackage: createServer('POST', '/accountbook/rest/bill/v1/accrual'),
 };
